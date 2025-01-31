d1170f6903d858c98bdec3cd72ac3f04d0e2a2af
Aanchal-kumari-R <aanchalkumari096@gmail.com>
2025-01-31 17:35:39 +0530

    Creating Expense Form

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render 
+from .forms import ExpenseForm
 
 # Create your views here.
 
-def index(request): 
-    return render(request,'myapp/index.html')
+def index(request):  
+    expense_form = ExpenseForm()
+    return render(request,'myapp/index.html',{'expense_form':expense_form})
